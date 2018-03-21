f2ee10022e1abc4c9e50d90a396822451c9ffb47
unknown <kincadealex@gmail.com>
2018-03-21 13:06:47 -0700

    code for exercise 1

--- ex1.py
+++ ex1.py
@@ -7,26 +7,20 @@
 # Print out that many copies of the previous message on separate lines. 
 # (Hint: the string "\n is the same as pressing the ENTER button)
 
-# ask user for their age
-# get age from user
-# store age in function
+# ask user for their name and store name
+# ask user for their age and store age
+name = input("Please tell me your name >>> ")
 age = int(input("Please tell me your age >>> "))
 
 # once you have the user's age, subtract it by 100 to see how much to add to the year
-age_100 = 100 - age
+year = str((2018 - age) + 100)
 
-# set the year to this current year and add 100 minus the age
-year = 2018 + age_100
-
-# print a message for the user telling them the year they will be 100 years old
-print("You will be 100 years old in %s" % year)
 
 # ask user for the amount of times they want to print the year
 # get the number from the user
 # store number of times in function
-times_to_print = int(input("How many times do you want to print that? >>> "))
+times_to_print = int(input("How many times do you want to print this? >>> "))
 
-# print a new line 
-# convert year function to a string
-# use the multiply operator to print the year
-print("\n %s" % str(year ) * times_to_print)
+# print a message for the user telling them the year they will be 100 years old 
+# print the message the amount specificed about of times
+age_100 = print("\n %s will be 100 years old in %s \n" % (name, year) * times_to_print)
